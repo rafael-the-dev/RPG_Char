d14767508530350aabb51e3db90d8512f32fd9c7
DiogoZ <diogozang2@gmail.com>
2021-08-16 22:05:00 -0300

    attempt to get data from the API with react

--- src/components/CharList.js
+++ src/components/CharList.js
@@ -10,34 +10,28 @@ function CharList() {
 
     
 
-// https://sheetdb.io/api/v1/t38d2ssfp8p6q
+//  https://sheetdb.io/api/v1/t38d2ssfp8p6q
 
 
     {/*REMOVER COMENTÁRIO ANTES DE USAR..*/}
 
-    client.read()
-    .then(async function(info) {
+    client.read().then(async function(info) {
         charList = await info;
         console.log(charList);
-
-    }, 
-    function(error){
-        console.log(error);
-    }
-);
-
-   
+    },      function(error){
+            console.log(error);
+    });
 
     return(
         <div className="created">
             <CharListItem
-                name="{charList[1].name}"
-                species="{charList[1].species}"
-                health="{charList[1].health}"
-                magic="{charList[1].magic}"
-                power="{charList[1].power}"
-                defense="{charList[1].defense}"
-                critical="{charList[1].critical}"
+                name={charList[1].name}
+                species={charList[1].species}
+                health={charList[1].health}
+                magic={charList[1].magic}
+                power={charList[1].power}
+                defense={charList[1].defense}
+                critical={charList[1].critical}
             />
         </div>
     );
