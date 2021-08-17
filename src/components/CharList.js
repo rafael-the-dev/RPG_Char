8e3e77f47f6a20c87b5355debb4bff06f402130d
DiogoZ <diogozang2@gmail.com>
2021-08-16 22:03:49 -0300

    attempt to get data from the API

--- src/components/CharList.js
+++ src/components/CharList.js
@@ -1,44 +1,44 @@
 import React from "react";
 import CharListItem from "./CharListItem";
 
+var charList = [];
+
 function CharList() {
 
     const sheetdb = require("sheetdb-node");
     const client = sheetdb({ address: 't38d2ssfp8p6q' });
 
-    var charList = {};
+    
+
 // https://sheetdb.io/api/v1/t38d2ssfp8p6q
 
-{/*
-    REMOVER COMENTÁRIO ANTES DE USAR..
+
+    {/*REMOVER COMENTÁRIO ANTES DE USAR..*/}
 
     client.read()
-    .then(function(info) {
-        console.log(info);
+    .then(async function(info) {
+        charList = await info;
+        console.log(charList);
+
     }, 
     function(error){
         console.log(error);
     }
 );
-*/
-}
-    
+
+   
 
     return(
         <div className="created">
-            
-           
-                
-                <CharListItem
-                    name="get name"
-                    species="???"
-                    health="00"
-                    magic="00"
-                    power="00"
-                    defense="00"
-                    critical="00"
-                />
-       
+            <CharListItem
+                name="{charList[1].name}"
+                species="{charList[1].species}"
+                health="{charList[1].health}"
+                magic="{charList[1].magic}"
+                power="{charList[1].power}"
+                defense="{charList[1].defense}"
+                critical="{charList[1].critical}"
+            />
         </div>
     );
 }
